fe942a743919f24e13fc431b47a39679880115c8
Dan Anderson <danfanderson@gmail.com>
2020-03-30 18:29:07 -0500

    Update shortcodes.go

--- shortcodes.go
+++ shortcodes.go
@@ -11,8 +11,8 @@ const charset = "0123456789" + "abcdefghijklmnopqrstuvwxyz" + "ABCDEFGHIJKLMNOPQ
 // Random process seed used to randomly create a shortlink code
 var seededRand = rand.New(rand.NewSource(time.Now().UnixNano()))
 
-// StringWithCharset creates a random string of characters with a specified length using a specified set of characters
-func StringWithCharset(length int, charset string) string {
+// GenShortCodeString creates a random string of characters with a specified length
+func GenShortCodeString(length int) string {
 	b := make([]byte, length)
 	for i := range b {
 		b[i] = charset[seededRand.Intn(len(charset))]
